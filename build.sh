3e5ad42d8f836e8d3a8f4ac4cc1672785e2c5d50
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-01-31 15:39:31 +0100

    Manual Setup.hs install

--- build.sh
+++ build.sh
@@ -2,6 +2,20 @@
 set -euo pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# USAGE: create_worktree NUM
+# Create an isolated working directory and copy the repository into it.
+# Intended to mimic Bazel's sandbox working directory.
+create_worktree() (
+  local NUM="$1"
+  local WORK="$SCRIPT_DIR/work$NUM"
+  rm -rf "$WORK"
+  mkdir "$WORK"
+  git archive --format=tar HEAD | tar x -C "$WORK"
+  echo "$WORK"
+)
+
+# USAGE: cc_build INSTALLDIR
+# Build and install the C components.
 cc_build() (
   local INSTALL="$1"
   cd myclib
@@ -12,30 +26,38 @@ cc_build() (
   install myclib.h libmyclib.so "$INSTALL/myclib"
 )
 
+# USAGE: cabal_build INSTALLDIR
+# Build and install the Cabal components.
 cabal_build() (
   local INSTALL="$1"
   cd mypackage
+  local RELINSTALL="$(realpath --relative-to="$PWD" "$INSTALL")"
   trap "runghc Setup.hs clean" EXIT
-  install -d "$INSTALL/package.conf.d"
+  ghc-pkg init "$INSTALL/package.conf.d"
   runghc Setup.hs configure \
     --user \
     --enable-deterministic \
     --enable-relocatable \
-    --extra-include-dirs="$(realpath --relative-to="$PWD" "$INSTALL/myclib")" \
-    --extra-lib-dirs="$(realpath --relative-to="$PWD" "$INSTALL/myclib")" \
+    --extra-include-dirs="$RELINSTALL/myclib" \
+    --extra-lib-dirs="$RELINSTALL/myclib" \
     --prefix="$INSTALL/mypackage" \
     --package-db="$INSTALL/package.conf.d"
   runghc Setup.hs build
-  runghc Setup.hs install
+  runghc Setup.hs copy
+  runghc Setup.hs register --gen-pkg-config="dist/mypackage-0.1.0.0.conf"
   # Patch package-db to be relocatable
-  sed -i -e "s#$INSTALL#\${pkgroot}#" "$INSTALL"/package.conf.d/*.conf
-  ghc-pkg recache --package-db="$INSTALL/package.conf.d"
+  sed -i -e "s#${RELINSTALL}#\${pkgroot}#" "dist/mypackage-0.1.0.0.conf"
+  ghc-pkg register --package-db "$INSTALL/package.conf.d" "dist/mypackage-0.1.0.0.conf"
 )
 
-rm -rf "$SCRIPT_DIR"/install*
+# USAGE: script WORKTREENUM
+# Create worktree NUM and build and install the C and Cabal components.
+script() (
+  local NUM="$1"
+  cd "$(create_worktree "$NUM")"
+  cc_build "$PWD/install"
+  cabal_build "$PWD/install"
+)
 
-cc_build  "$SCRIPT_DIR/install1"
-cabal_build "$SCRIPT_DIR/install1"
-
-cc_build  "$SCRIPT_DIR/install2"
-cabal_build "$SCRIPT_DIR/install2"
+script 1
+script 2
